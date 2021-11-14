f707fd9741f58fb3a9d08e7bfb987e9c3e91b702
Alexey Hohlov <root@amper.me>
2021-11-14 20:46:35 +0300

    Поля в протоколе VESC в формате big-endian

--- src/app/motion.c
+++ src/app/motion.c
@@ -6,12 +6,35 @@
  * @brief Функции управления движением
  */
 
+#include <byteswap.h>
 #include <io/canbus.h>
 #include <log/log.h>
 #include <svc/sharedmem.h>
 #include <svc/svc.h>
 
 #include <private/motion.h>
+
+#define bswap16(x) ((x) >> 8 | ((x)&255) << 8)
+
+static inline int32_t
+bswap32(int32_t x)
+{
+	union {
+		const int32_t *i;
+		const uint32_t *u;
+	} u1;
+
+	union {
+		int32_t i;
+		uint32_t u;
+	} u2;
+
+	u1.i = &x;
+
+	u2.u = __bswap_constant_32(*u1.u);
+
+	return u2.i;
+}
 
 static void
 parse_msg(const struct can_packet_t *msg)
@@ -28,9 +51,9 @@ parse_msg(const struct can_packet_t *msg)
 		} u;
 
 		u.p8 = msg->data;
-		log_inf("rpm: %i, current: %.1f, duty: %.3f", u.status->rpm,
-			(double)u.status->current_X10 / 10.0,
-			(double)u.status->duty_X1000 / 1000.0);
+		log_inf("rpm: %i, current: %.1f, duty: %.3f", bswap32(u.status->rpm),
+			(double)bswap16(u.status->current_X10) / 10.0,
+			(double)bswap16(u.status->duty_X1000) / 1000.0);
 		break;
 	}
 
@@ -45,8 +68,8 @@ parse_msg(const struct can_packet_t *msg)
 
 		u.p8 = msg->data;
 		log_inf("consumed: %.4f ah, charged: %.4f ah",
-			(double)u.status2->ah_X10000 / 10000.0,
-			(double)u.status2->ahch_X10000 / 10000.0);
+			(double)bswap32(u.status2->ah_X10000) / 10000.0,
+			(double)bswap32(u.status2->ahch_X10000) / 10000.0);
 		break;
 	}
 
@@ -61,8 +84,8 @@ parse_msg(const struct can_packet_t *msg)
 
 		u.p8 = msg->data;
 		log_inf("consumed: %.4f wh, charged: %.4f wh",
-			(double)u.status3->wh_X10000 / 10000.0,
-			(double)u.status3->whch_X10000 / 10000.0);
+			(double)bswap32(u.status3->wh_X10000) / 10000.0,
+			(double)bswap32(u.status3->whch_X10000) / 10000.0);
 		break;
 	}
 
@@ -79,10 +102,10 @@ parse_msg(const struct can_packet_t *msg)
 
 		u.p8 = msg->data;
 		log_inf("temp_fet: %.1f, temp_motor: %.1f, current_in: %.1f, pid_pos: %.2f",
-			(double)u.status4->temp_fet_X10 / 10.0,
-			(double)u.status4->temp_motor_X10 / 10.0,
-			(double)u.status4->current_in_X10 / 10.0,
-			(double)u.status4->pid_pos_now_X50 / 50.0);
+			(double)bswap16(u.status4->temp_fet_X10) / 10.0,
+			(double)bswap16(u.status4->temp_motor_X10) / 10.0,
+			(double)bswap16(u.status4->current_in_X10) / 10.0,
+			(double)bswap16(u.status4->pid_pos_now_X50) / 50.0);
 		break;
 	}
 
@@ -97,8 +120,8 @@ parse_msg(const struct can_packet_t *msg)
 		} u;
 
 		u.p8 = msg->data;
-		log_inf("tacho: %i, v_in: %.1f", u.status5->tacho_value,
-			(double)u.status5->v_in_X10 / 10.0);
+		log_inf("tacho: %i, v_in: %.1f", bswap32(u.status5->tacho_value),
+			(double)bswap16(u.status5->v_in_X10) / 10.0);
 		break;
 	}
 
